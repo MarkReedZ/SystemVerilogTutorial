92815e7415efea7dae65c3a2db6b23f2869fc9d6
Mark Reed <mreed@us.ibm.com>
2021-11-08 13:58:13 -0500

    cleanup

--- det/shm.tcl
+++
@@ -1,6 +0,0 @@
-
-database -open waves -shm
-probe -create tb -depth all -all -memories -shm -database waves
-run
-exit
-
--- det/xcelium.d/run.lnx86.19.07.d/files.ts
+++
@@ -1,2 +0,0 @@
-1635871604 /home/mreed/uvm/det/top.sv
-1635872241 /home/mreed/uvm/det/detector.sv
